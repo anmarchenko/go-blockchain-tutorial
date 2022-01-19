c674dad54700eaf097876b68fa57fba8b11074bb
Andrey Marchenko <andrey.marchenko@share-now.com>
2022-01-19 11:33:36 +0100

    proof of work

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -11,9 +12,11 @@ func main() {
 	bc.AddBlock("Send 2 more BTC to John")
 
 	for _, block := range bc.blocks {
+		pow := NewProofOfWork(block)
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
